013442223940e0e2b28701da60cf997a929d6a5c
Kyle <prophet@hanyang.ac.kr>
2023-06-14 18:11:26 +0900

    [Add 404 Page, Humans and Robots] - 404.html - humans.txt - robots.txt

--- src/main/java/kr/or/sw/model/UsrDTO.java
+++ src/main/java/kr/or/sw/model/UsrDTO.java
@@ -1,13 +1,20 @@
 package kr.or.sw.model;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class UsrDTO {
 
     private String account; // 계정, PK
     private String password;    // 비밀번호
     private String salt;    // salt
-    private boolean isAdmin;    // 관리자 여부
+    private boolean admin;    // 관리자 여부
     private int empNo;   // 사번, FK
+
+    public UsrDTO(String account, String password) {
+        this.account = account;
+        this.password = password;
+    }
 }
