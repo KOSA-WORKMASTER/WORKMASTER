5dca501e30389a5aa7c413a5225978f88fd1e7cc
Kyle <prophet@hanyang.ac.kr>
2023-06-20 09:19:06 +0900

    Add DAO Interface - AuthDAO.java - AuthDAOImpl.java

--- src/main/java/kr/or/sw/mapper/AuthDAO.java
+++ src/main/java/kr/or/sw/mapper/AuthDAO.java
@@ -1,26 +1,9 @@
 package kr.or.sw.mapper;
 
 import kr.or.sw.model.UsrDTO;
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.session.SqlSession;
 
-@Slf4j
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class AuthDAO {
+public interface AuthDAO {
 
-    private static final AuthDAO INSTANCE = new AuthDAO();
-
-    public static AuthDAO getINSTANCE() {
-        return INSTANCE;
-    }
-
-    public int checkCredentials(SqlSession sqlSession, UsrDTO usrDTO) {
-        log.info("checkCredentials()");
-
-        // 여기에 데이터베이스 작업을 수행하는 쿼리 작성
-        // 예시: 사용자명과 암호의 일치여부를 반환하는 쿼리
-        return sqlSession.selectOne("checkCredentials", usrDTO);
-    }
+    int checkCredentials(SqlSession sqlSession, UsrDTO usrDTO);
 }
