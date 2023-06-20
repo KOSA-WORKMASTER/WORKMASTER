898072172e07783e0743e9645d7370431c59fac5
Kyle <prophet@hanyang.ac.kr>
2023-06-20 10:06:36 +0900

    Add Password Encryption - CipherUtil.java

---
+++ src/main/java/kr/or/sw/util/CipherUtil.java
@@ -0,0 +1,52 @@
+package kr.or.sw.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+@Slf4j
+@NoArgsConstructor(access = AccessLevel.PRIVATE)    // Singleton
+public class CipherUtil {
+
+    private static final int SALT_LENGTH = 16;
+    private static final int ITERATIONS = 10000;
+    private static final int KEY_LENGTH = 512;
+
+    private static final SecureRandom secureRandom = new SecureRandom();
+
+    private static final CipherUtil INSTANCE = new CipherUtil();
+
+    public static CipherUtil getInstance() {
+        return INSTANCE;
+    }
+
+    public String generateSalt() {
+        byte[] salt = new byte[SALT_LENGTH];
+        secureRandom.nextBytes(salt);
+        return Base64.getEncoder().encodeToString(salt);
+    }
+
+    public String hashPassword(String password, String salt) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-" + KEY_LENGTH);
+            byte[] saltBytes = Base64.getDecoder().decode(salt);
+            digest.reset();
+            digest.update(saltBytes);
+            byte[] hash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+            for (int i = 0; i < ITERATIONS; i++) {
+                digest.reset();
+                hash = digest.digest(hash);
+            }
+            return Base64.getEncoder().encodeToString(hash);
+        } catch (NoSuchAlgorithmException e) {
+            log.error("해싱 과정에서 에러 발생: {}", e.getMessage());
+            return null;
+        }
+    }
+}
