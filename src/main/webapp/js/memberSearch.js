8ce8e2f9bde119e6b15436f463da0a4277a02ccc
bnbu <jsh031613@naver.com>
2023-07-04 17:18:37 +0900

    docs: add comment

--- src/main/webapp/js/memberSearch.js
+++ src/main/webapp/js/memberSearch.js
@@ -5,3 +5,21 @@ $("#search-form").submit((e) => {
     }
     return true;
 })
+
+// jquery에는 forEach 아니고 each인듯
+$(".member-data").each((index, element) => {
+    $(element).click(() => {
+        // console.log(element.getAttribute("id").substring(10));
+
+        // 클릭한 데이터의 인덱스
+        let elementIdx = parseInt(element.getAttribute("id").substring(10));
+        
+        // 현재 선택한 방식
+        // 1:수정 2:삭제
+        let type = $('input[name=select-type]:checked').val()
+        
+        /* 
+           두 변수를 적절히 사용하여 수정 혹은 삭제를 진행하는 코드를 작성할 것
+         */
+    });
+})
