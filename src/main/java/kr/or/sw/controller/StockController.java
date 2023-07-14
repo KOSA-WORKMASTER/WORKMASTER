5e3d6a0ef9de96e4a2806fa5c4068e8096720648
mkshim <myungaa0626@gmail.com>
2023-07-14 15:23:55 +0900

    add stock (insert), update (index.js) - stockController - stockDAO - stockDTO - stockServiceImpl - stockInsert.jsp - index.js

--- src/main/java/kr/or/sw/controller/StockController.java
+++ src/main/java/kr/or/sw/controller/StockController.java
@@ -16,88 +16,94 @@ import static kr.or.sw.controller.HomeController.HOME_PATH;
 import static kr.or.sw.controller.HomeController.handleInvalidAccess;
 
 @Slf4j
-@WebServlet(name = "StockController", urlPatterns = {"/stock/*"})
+@WebServlet(name = "StockController", urlPatterns = { "/stock/*" })
 public class StockController extends HttpServlet {
 
-    @Serial
-    private static final long serialVersionUID = 4974481185726010330L;
+	@Serial
+	private static final long serialVersionUID = 4974481185726010330L;
 
-    private StockService stockService;
+	private StockService stockService;
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        log.info("doGet()");
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		log.info("doGet()");
 
-        String pathInfo = request.getPathInfo();
-        switch (pathInfo) {
-            case "/list" -> {
-                log.info("/list");
-                handleSearch(request, response);
-            }
-            case "/insert" -> {
-                log.info("/insert");
-            }
-            case "/update" -> {
-                log.info("/update");
-                stockService.select(request, response);
-            }
-            default -> handleInvalidAccess(request, response);
-        }
-        request.setAttribute("path", request.getRequestURI().substring(request.getContextPath().length()));
-        request.getRequestDispatcher(request.getContextPath() + HOME_PATH).forward(request, response);
-    }
+		String pathInfo = request.getPathInfo();
+		switch (pathInfo) {
+		case "/list" -> {
+			log.info("/list");
+			handleSearch(request, response);
+		}
+		case "/insert" -> {
+			log.info("/insert");
+		}
+		case "/update" -> {
+			log.info("/update");
+			stockService.select(request, response);
+		}
+		default -> handleInvalidAccess(request, response);
+		}
+		request.setAttribute("path", request.getRequestURI().substring(request.getContextPath().length()));
+		request.getRequestDispatcher(request.getContextPath() + HOME_PATH).forward(request, response);
+	}
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        log.info("doPost()");
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		log.info("doPost()");
 
-        response.setContentType("application/json");
-        String pathInfo = request.getPathInfo();
-        switch (pathInfo) {
+		response.setContentType("application/json");
+		String pathInfo = request.getPathInfo();
+		switch (pathInfo) {
 
-            case "/delete" -> {
-                log.info("/delete");
-                if (stockService.delete(request, response)) log.info("재고정보 삭제 성공");
-                request.getRequestDispatcher(request.getContextPath() + HOME_PATH).forward(request, response);
-            }
-            case "/insert" -> {
-                log.info("/insert");
-                if (stockService.insert(request, response)) log.info("재고 추가 성공");
-                request.setAttribute("path", "/stock/list");
-            }
-            case "/update" -> {
-                log.info("/update");
-                if (stockService.update(request, response)) log.info("재고 수정 성공");
-                response.sendRedirect("/stock/list"); // stock/list
-            }
-            default -> handleInvalidAccess(request, response);
-        }
-    }
+		case "/delete" -> {
+			log.info("/delete");
+			if (stockService.delete(request, response))
+				log.info("재고정보 삭제 성공");
+			request.getRequestDispatcher(request.getContextPath() + HOME_PATH).forward(request, response);
+		}
+		case "/insert" -> {
+			log.info("/insert");
+			if (stockService.insert(request, response))
+				log.info("재고 추가 성공");
+			response.sendRedirect("/stock/list");
+		}
+		case "/update" -> {
+			log.info("/update");
+			if (stockService.update(request, response))
+				log.info("재고 수정 성공");
+			response.sendRedirect("/stock/list"); // stock/list
+		}
+		default -> handleInvalidAccess(request, response);
+		}
+	}
 
-    @Override
-    public void init() throws ServletException {
-        log.info("/stock/*");
-        stockService = StockServiceImpl.getInstance();
-    }
+	@Override
+	public void init() throws ServletException {
+		log.info("/stock/*");
+		stockService = StockServiceImpl.getInstance();
+	}
 
-    @Override
-    public void destroy() {
-        log.info("destroy()");
-    }
+	@Override
+	public void destroy() {
+		log.info("destroy()");
+	}
 
-    private void handleSearch(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setCharacterEncoding("UTF-8");
+	private void handleSearch(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
 
-        int searchOption = 0;
-        if (request.getParameter("searchOption") != null) {
-            searchOption = Integer.parseInt(request.getParameter("searchOption"));
-        }
-        // searchOption이 0이면 전체 검색, 0이 아닌 다른 무언가면 그에 해당하는 검색을 진행
-        switch (searchOption) {
+		int searchOption = 0;
+		if (request.getParameter("searchOption") != null) {
+			searchOption = Integer.parseInt(request.getParameter("searchOption"));
+		}
+		// searchOption이 0이면 전체 검색, 0이 아닌 다른 무언가면 그에 해당하는 검색을 진행
+		switch (searchOption) {
 
-            case 0 -> stockService.selectAll(request, response);
-            case 1, 2, 3 -> stockService.searchBy(request, response);
-            default -> handleInvalidAccess(request, response);
-        }
-    }
+		case 0 -> stockService.selectAll(request, response);
+		case 1, 2, 3 -> stockService.searchBy(request, response);
+		default -> handleInvalidAccess(request, response);
+		}
+	}
 }
--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -11,3 +11,66 @@ function switchForm(selectedForm) {
         adminForm.style.display = 'block';
     }
 }
+
+const clientLoginForm = document.querySelector("#client-login");
+
+clientLoginForm.addEventListener("submit", function(e) {
+    const emailInput = clientLoginForm.querySelector("#email");
+    const passwordInput = clientLoginForm.querySelector("#password");
+
+    if (!emailInput.value) {
+        alert("아이디를 입력해주세요.");
+        e.preventDefault(); // 폼 제출 막기
+    }
+    else if (!passwordInput.value) {
+        alert("비밀번호를 입력해주세요.");
+        e.preventDefault(); // 폼 제출 막기
+    }
+});
+
+const managerLoginForm = document.querySelector("#manager-login");
+
+managerLoginForm.addEventListener("submit", function(e) {
+    const adminIdInput = managerLoginForm.querySelector("#adminId");
+    const adminPwInput = managerLoginForm.querySelector("#adminPw");
+
+    if (!adminIdInput.value) {
+        alert("관리자 아이디를 입력해주세요.");
+        e.preventDefault(); // 폼 제출 막기
+    }
+    else if (!adminPwInput.value) {
+        alert("관리자 비밀번호를 입력해주세요.");
+        e.preventDefault(); // 폼 제출 막기
+    }
+});
+
+// 밑에 스크립트는 index 에 적용되게 수정해야됨 
+
+$("#member-login-confirm").click((e) => {
+
+
+	
+    if (!emailChk) {
+        $.ajax({
+            url: "/auth/checkEmail",
+            type: "post",
+            data: {
+                email: $("#email").val()
+            },
+            dataType: "json",
+            success: (res) => {
+                if (!res) {
+
+                    emailChk = true;
+//                    alert("아이디 또는 비밀번호가 틀렸습니다.");
+                } else {
+                    alert("아이디 또는 비밀번호가 틀렸습니다.");
+                    return;
+                }
+            }
+        });
+    } 
+   
+});
+
+
